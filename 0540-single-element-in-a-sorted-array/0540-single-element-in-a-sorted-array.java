2158e11b93b95515c03f45fedc56f36f01929858
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-02-21 20:31:08 +0530

    Time: 0 ms (100.00%), Space: 61 MB (5.34%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -10,5 +10,6 @@ class Solution {
             else hi=mid;
         }
         return nums[lo];
+ //https://leetcode.com/problems/single-element-in-a-sorted-array/discuss/627921/Java-or-C%2B%2B-or-Python3-or-Easy-explanation-or-O(logn)-or-O(1)       
     }
 }
