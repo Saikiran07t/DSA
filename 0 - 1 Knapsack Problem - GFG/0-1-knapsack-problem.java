5b82a13bcf42eea03a4e5ef4ce60f1c070fa9915
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-03-15 20:04:52 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -49,76 +49,82 @@ class gfg
 class Solution 
 { 
     //Function to return max value that can be put in knapsack of capacity W.
-    // static int knapSack(int W, int wt[], int val[], int n) 
-    // { 
-         // your code here 
-        //   int[][] dp = new int[n+1][W+1];
-
-        //  for(int[] row : dp)
-        //      Arrays.fill(row, -1);
-        //       // base case
-        //     if(n==0 || W==0)
-        //       return 0;
-
-        //     if(dp[n][W]!=-1)  return dp[n][W];
-
-        //     if(wt[n-1]<=W)
-        //       return dp[n][W]=Math.max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1),
-        //       knapSack(W,wt,val,n-1));
-        //   //  if(weight[n-1]>maxWeight)
-        //  return dp[n][W]= knapSack(W,wt,val,n-1);
-
-
-
-    //} 
     
-     static int max(int a, int b) { return (a > b) ? a : b; }
- 
-    // Returns the value of maximum profit
-    static int knapSackRec(int W, int wt[], int val[],
-                           int n, int[][] dp)
-    {
- 
-        // Base condition
-        if (n == 0 || W == 0)
-            return 0;
- 
-        if (dp[n][W] != -1)
-            return dp[n][W];
- 
-        if (wt[n - 1] > W)
- 
-            // Store the value of function call
-            // stack in table before return
-            return dp[n][W]
-                = knapSackRec(W, wt, val, n - 1, dp);
- 
-        else
- 
-            // Return value of table after storing
-            return dp[n][W]
-                = max((val[n - 1]
-                       + knapSackRec(W - wt[n - 1], wt, val,
-                                     n - 1, dp)),
-                      knapSackRec(W, wt, val, n - 1, dp));
-    }
- 
-    static int knapSack(int W, int wt[], int val[], int N)
-    {
- 
-        // Declare the table dynamically
-        int dp[][] = new int[N + 1][W + 1];
- 
-        // Loop to initially filled the
-        // table with -1
-        for (int i = 0; i < N + 1; i++)
-            for (int j = 0; j < W + 1; j++)
-                dp[i][j] = -1;
- 
-        return knapSackRec(W, wt, val, N, dp);
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        //  your code here 
+      int[][] dp = new int[n+1][W+1];
+       for(int[] row : dp)
+          Arrays.fill(row, -1);
+          
+        return knapSackUtil(wt,val,dp,W,n);
     }
     
-    
+    static int knapSackUtil(int wt[],int val[],int[][] dp,int W, int n){
+              // base case
+            if(n==0 || W==0)
+                return 0;
+
+            if(dp[n][W]!=-1)  return dp[n][W];
+
+            if(wt[n-1]<=W)
+              return dp[n][W]=Math.max(val[n-1]+knapSackUtil(wt,val,dp,W-wt[n-1],n-1),
+              knapSackUtil(wt,val,dp,W,n-1));
+         // if(weight[n-1]>maxWeight)  
+          return dp[n][W]= knapSackUtil(wt,val,dp,W,n-1);
+
+
+
+    } 
 }
+    
+    //  static int max(int a, int b) { return (a > b) ? a : b; }
+ 
+    // // Returns the value of maximum profit
+    // static int knapSackRec(int W, int wt[], int val[],
+    //                       int n, int[][] dp)
+    // {
+ 
+    //     // Base condition
+    //     if (n == 0 || W == 0)
+    //         return 0;
+ 
+    //     if (dp[n][W] != -1)
+    //         return dp[n][W];
+ 
+    //     if (wt[n - 1] > W)
+ 
+    //         // Store the value of function call
+    //         // stack in table before return
+    //         return dp[n][W]
+    //             = knapSackRec(W, wt, val, n - 1, dp);
+ 
+    //     else
+ 
+    //         // Return value of table after storing
+    //         return dp[n][W]
+    //             = max((val[n - 1]
+    //                   + knapSackRec(W - wt[n - 1], wt, val,
+    //                                  n - 1, dp)),
+    //                   knapSackRec(W, wt, val, n - 1, dp));
+    // }
+ 
+    // static int knapSack(int W, int wt[], int val[], int N)
+    // {
+ 
+    //     // Declare the table dynamically
+    //     int dp[][] = new int[N + 1][W + 1];
+ 
+    //     // Loop to initially filled the
+    //     // table with -1
+    //     for (int i = 0; i < N + 1; i++)
+    //         for (int j = 0; j < W + 1; j++)
+    //             dp[i][j] = -1;
+ 
+    //     return knapSackRec(W, wt, val, N, dp);
+    // }
+    
+    
+// }
 
 
