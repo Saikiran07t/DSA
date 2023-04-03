7d244a657ae37a53bdab070a8750ba914d008e89
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-04-03 21:32:45 +0530

    Time: 17 ms (98.31%), Space: 51.1 MB (15.64%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,4 +1,25 @@
 class Solution {
+    
+    /*
+    Approach
+Sort the people array in non-decreasing order.
+
+Initialize the left pointer to the first element of the array, and the right pointer to the last element of the array.
+
+Initialize a counter variable boats to 0.
+
+While the left pointer is less than or equal to the right pointer, do the following:
+a. Check if the sum of the weights of the people at the left and right pointers is less than
+or equal to the limit.
+b. If the sum is less than or equal to the limit, move the left pointer one step to the
+right to include the next lightest person.
+c. Move the right pointer one step to the left to include the next heaviest person,
+regardless of whether or not they can fit on the current boat.
+d. Increment the boats counter by 1.
+
+Return the boats counter, which represents the total number of boats used.
+    
+    */
     public int numRescueBoats(int[] people, int limit) {
          int boatCount = 0;
         Arrays.sort(people);
