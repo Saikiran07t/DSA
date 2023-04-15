7545b6c1e21675af244a947c3f670aaf83230f87
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-04-15 15:05:35 +0530

    Time: 391 ms (8.64%), Space: 45.1 MB (70.99%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,7 +1,28 @@
 class Solution {
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-         //Knapsack variation
-        int[] f = new int[k + 1];
+          int n = piles.size();
+        int dp[][] = new int[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int coins = 0; coins <= k; coins++) {
+                int currentSum = 0;
+                for (int currentCoins = 0; currentCoins <= Math.min((int)piles.get(i - 1).size(), coins); currentCoins++) {
+                    if (currentCoins > 0) {
+                        currentSum += piles.get(i - 1).get(currentCoins - 1);
+                    }
+                    dp[i][coins] = Math.max(dp[i][coins], dp[i - 1][coins - currentCoins] + currentSum);
+                }
+            }
+        }
+        return dp[n][k];
+        
+        
+        
+        
+        
+        
+        
+        //Knapsack variation
+      /*  int[] f = new int[k + 1];
         int sumN = 0;
         for (var pile : piles) {
             int n = pile.size();
@@ -17,6 +38,6 @@ class Solution {
             }
         }
 
-        return f[k];
+        return f[k];*/
     }
 }
