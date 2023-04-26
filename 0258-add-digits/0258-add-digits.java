1e7c30d320700c611673b4424418482ee2545818
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-04-26 23:08:49 +0530

    Time: 1 ms (100.00%), Space: 39.9 MB (56.73%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,57 @@
 class Solution {
     public int addDigits(int num) {
+        //Recursion 
         //  Variable to store the digit.
-    int ans = 0;
-    // Calculation of digit sum.
-    while(num!=0) {
-        ans += (num % 10);
-        num /= 10;
-    }
-    // If  ans is less than 10, it is the digital root. Return it.
-    if(ans < 10)
-        return ans;
-    else
-        return addDigits(ans);  // ans becomes the new number. Call the function again with ans as parameter.
+    // int ans = 0;
+    // // Calculation of digit sum.
+    // while(num!=0) {
+    //     ans += (num % 10);
+    //     num /= 10;
+    // }
+    // // If  ans is less than 10, it is the digital root. Return it.
+    // if(ans < 10)
+    //     return ans;
+    // else
+    //     return addDigits(ans);  // ans becomes the new number. Call the function again with ans as parameter.
+        
+        
+        //Recursion using string
+      //  Variable to store the digit sum...
+        // int sum = 0;
+        // // Converts num value into string...
+        // String s = String.valueOf(num);
+        // // Calculation of digit sum...
+        // for(int i = 0; i < s.length(); i++){
+        //     sum = sum + (s.charAt(i) - '0');
+        // }
+        // // If  sum is less than 10, it is the digital root. Return it...
+        // if(sum < 10){
+        //     return sum;
+        // }else{
+        //     return addDigits(sum);      // Sum becomes the new number. Call the function again with sum as parameter...
+        // }
+        
+    
+  /*
+    
+    Any number where it's digits add to 9 is always divisible by 9. (18, 27, 36, 45, 54, 63, 72, 81, 90, etc.) Therefore the 'digital root' for any number divisible by 9 is always 9. You can see this even in larger numbers like 99 because 9 + 9 = 18, and then 1 + 8 = 9 still, so the root always becomes 9 for any numbers divisible by 9.
+
+Additionally, 0 always has a digital root of 0 obviously.
+
+The only other cases you need to worry about to find the digital root are when it isn't 0 or 9.
+
+So for any number that isn't 0 and isn't divisible by 9, the root will always n % 9 for a given number n. (AKA the difference between given number n and the nearest number that is divisible by 9, since numbers divisible by 9 always have a digital root of 9).
+For examples: 100 % 9 = 1 (one greater than 99, which is divisible by 9).
+101 % 9 = 2
+102 % 9 = 3 and so on.
+
+This explanation/algorithm skips the whole "add digits until there is only 1 remaining", so the description of this problem seems pretty misleading to me since it makes you think the solution will be something unrelated to the optimal one. I guess the point of Leetcode is to learn all of these tricks though.
+   
+  */
+        
+         if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
+              
     }
 }
