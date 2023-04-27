d419afea71b980017dbb9fc46bbade24bb577ee9
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-04-27 21:21:36 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (68.86%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,4 +1,15 @@
 class Solution {
+    /*
+    *After the ith round, every ith bulb is toggled.
+
+So, for example, the 20th bulb is toggled in the following rounds : 1,2,4,5,10,20.
+The 25th bulb is toggled every 1st,5th and in the 25th round.
+
+Notice that the number of times each bulb is toggled is equal to the number of its factors.
+Also notice that if we toggle a bulb even number of times, it goes back to its original OFF state.
+
+Since every number,except perfect squares, has an even number of factors, the number of bulbs in the ON state after n toggles will be equal to the number of perfect squares <= n.
+    */
     public int bulbSwitch(int n) {
         return (int) Math.sqrt(n);
     }
