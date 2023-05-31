c7ba3f7fc765a957eecb1e80f30e8d1aebd670fb
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-05-31 23:27:05 +0530

    Time: 1 ms (98.33%), Space: 43.3 MB (9.18%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -10,7 +10,7 @@ class Solution {
        */ 
         
         //optimal Hashing
-         Map<Integer,Integer> map = new HashMap<>();
+       /*  Map<Integer,Integer> map = new HashMap<>();
         int res = 0;
         
         for(int i = 0;i< nums.length;i++){
@@ -23,7 +23,21 @@ class Solution {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         
+        return res;
+        */
         
+        //count sort
+         int res = 0;
+        int[] freq = new int[101];
+        for(int i=0;i<nums.length;i++){
+            freq[nums[i]]++;
+        }
+        // for(int i=freq.length-1;i>=k;i--){
+        for(int i=k+1;i<freq.length;i++){
+            // if(freq[i]>0 && freq[i-k]>0){
+                res+=freq[i]*freq[i-k];
+            // }
+        }
         return res;
     }
 }
