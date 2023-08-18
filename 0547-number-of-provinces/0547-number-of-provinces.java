4cedfab3298ef9856ae121d00e93a6a27df3c9f8
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-08-18 16:24:15 +0530

    Time: 4 ms (23.39%), Space: 45.9 MB (73.51%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int noOfProvinces=0;
+        List<List<Integer>> adjList = new ArrayList<>();
+        int n= isConnected.length;
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
+              for(int j=0;j<n;j++){
+            if(isConnected[i][j]==1 && i!=j){
+                adjList.get(i).add(j);
+            }
+          }
+        }
+        
+        boolean[] visited = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                bfs(i,adjList,visited);
+                noOfProvinces++;
+            }
+        }
+        return noOfProvinces;
+      
+    }
+    
+    public void bfs(int vertex,List<List<Integer>> adjList,boolean visited[]){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(vertex);
+        visited[vertex]=true;
+        
+        while(!queue.isEmpty()){
+            int cur=queue.poll();
+            for(int neighbor : adjList.get(cur)){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true;
+                    queue.add(neighbor);   
+                }
+            }
+        }
+    }
+}
