0a3473bfa56a7c76d387306fafca48dd606e9a52
Sai Kiran <68822297+Saikiran07t@users.noreply.github.com>
2023-08-19 16:15:48 +0530

    Time: 4 ms (75.55%), Space: 62.1 MB (67.62%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,20 +15,28 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
+        //using BFS
+        // if(root == null) return 0;
+        // Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // queue.add(root);
+        // int depth=1;
+        // while(!queue.isEmpty()){
+        //     int size=queue.size();
+        //     for(int i=0;i<size;i++){
+        //         TreeNode cur=queue.poll();
+        //         if(cur.left== null && cur.right==null) return depth;
+        //         if(cur.left!=null) queue.add(cur.left);
+        //         if(cur.right!=null) queue.add(cur.right);
+        //     }
+        //     depth++;
+        // }
+        // return depth;
+        
+        //using DFS
         if(root == null) return 0;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        int depth=1;
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode cur=queue.poll();
-                if(cur.left== null && cur.right==null) return depth;
-                if(cur.left!=null) queue.add(cur.left);
-                if(cur.right!=null) queue.add(cur.right);
-            }
-            depth++;
-        }
-        return depth;
+        if(root.left==null && root.right == null) return 1;
+        int leftHeight = root.left!=null ?minDepth(root.left) : Integer.MAX_VALUE;
+        int rightHeight = root.right!=null? minDepth(root.right): Integer.MAX_VALUE;
+        return 1+Math.min(leftHeight,rightHeight);
     }
 }
